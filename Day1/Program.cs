9e40719046e11e6b952d0ccb82b19810052bcfa7
Leon Grave <l.grave@gmail.com>
2022-12-10 11:21:40 +0100

    Finished day1

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿//https://adventofcode.com/2022/day/1
+
+var input = File.ReadAllLines("input.txt");
+
+var caloriesPerElf = new Dictionary<int, int>();
+
+int elf = 1;
+caloriesPerElf.Add(elf, 0);
+foreach(var line in input) {
+
+    if(string.IsNullOrWhiteSpace(line)) {
+        elf++;
+        caloriesPerElf.Add(elf, 0);
+    }
+    else {
+        caloriesPerElf[elf] += int.Parse(line);
+    }
+}
+
+var elfWithMostCalories = caloriesPerElf.MaxBy(x => x.Value);
+
+Console.WriteLine($"Elf {elfWithMostCalories.Key} has the most calories: {elfWithMostCalories.Value}");
+
+//Elf 31 has the most calories: 66306
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aoc2022
 Advent of Code 2022
+
+# Introduction
+
+This is where I try to solve the Advent of Code (https://adventofcode.com) 2022 puzzles using C#.
