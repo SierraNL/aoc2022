492140c8e368d6a99ab1ef251c73ac5149daea5a
Leon Grave <l.grave@gmail.com>
2022-12-11 16:57:52 +0100

    Day 4 part 1

---
+++ Day4/Program.cs
@@ -0,0 +1,39 @@
+﻿//https://adventofcode.com/2022/day/4
+
+var input = File.ReadAllLines("input.txt");
+int totalConveringPairs = 0;
+
+foreach(var pair in input) {
+    var elf1 = GetRange(pair.Split(',')[0]);
+    var elf2 = GetRange(pair.Split(',')[1]);
+
+    if(IsCovering(elf1, elf2)) {
+        totalConveringPairs++;
+    }
+}
+
+Console.WriteLine($"The assignments contain {totalConveringPairs} covering pairs of elfs.");
+
+IEnumerable<int> GetRange(string input) {
+    var result = new List<int>();
+
+    var start = int.Parse(input.Split('-')[0]);
+    var end = int.Parse(input.Split('-')[1]);
+
+    for(int i = start; i<=end; i++) {
+        result.Add(i);
+    }
+
+    return result;
+}
+
+bool IsCovering(IEnumerable<int> elf1, IEnumerable<int> elf2) {
+    bool result = false;
+
+    if(elf1.All(x => elf2.Contains(x)) || elf2.All(x => elf1.Contains(x))) {
+        Console.WriteLine($"Pairs {string.Join(",", elf1)} and {string.Join(",", elf2)} are covering pairs of elfs.");
+        result = true;
+    }
+
+    return result;
+}
