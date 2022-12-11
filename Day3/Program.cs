c15b97348d6519c957cbe7ad2825568d460aa565
Leon Grave <l.grave@gmail.com>
2022-12-11 09:53:08 +0100

    Day 3 part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,36 @@
+﻿//https://adventofcode.com/2022/day/3
+
+var input = File.ReadAllLines("input.txt");
+
+int prioritiesSum = 0;
+
+foreach(var rucksack in input) {
+    //split into compartments
+    var compartment1 = rucksack.Substring(0, rucksack.Length / 2);
+    var compartment2 = rucksack.Substring((rucksack.Length / 2), rucksack.Length / 2);
+
+    //find doubles
+    string itemsAlreadyFound = string.Empty;
+    foreach(var itemType in compartment1) {
+        if(compartment2.Contains(itemType) && !itemsAlreadyFound.Contains(itemType)) {
+            //add to already found
+            itemsAlreadyFound += itemType;
+            //convert to ascii
+            var asciiValue = (int)itemType;
+            
+            //lowercase correction to priority
+            if(asciiValue > 96) {
+                prioritiesSum += asciiValue - 96;
+                Console.WriteLine($"Found {itemType} convert to in {asciiValue} corrected for priority {asciiValue - 96}");
+            }
+            //uppercase correction to priority
+            else {
+                prioritiesSum += asciiValue - 38;
+                Console.WriteLine($"Found {itemType} convert to in {asciiValue} corrected for priority {asciiValue - 38}");
+            }
+        }
+    }
+
+}
+
+Console.WriteLine($"The sum of all priorities is {prioritiesSum}");
