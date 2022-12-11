e9690e273f26b2f5f3f05456575a2e43661466c8
Leon Grave <l.grave@gmail.com>
2022-12-11 10:24:42 +0100

    Day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -15,22 +15,56 @@ foreach(var rucksack in input) {
         if(compartment2.Contains(itemType) && !itemsAlreadyFound.Contains(itemType)) {
             //add to already found
             itemsAlreadyFound += itemType;
-            //convert to ascii
-            var asciiValue = (int)itemType;
-            
-            //lowercase correction to priority
-            if(asciiValue > 96) {
-                prioritiesSum += asciiValue - 96;
-                Console.WriteLine($"Found {itemType} convert to in {asciiValue} corrected for priority {asciiValue - 96}");
-            }
-            //uppercase correction to priority
-            else {
-                prioritiesSum += asciiValue - 38;
-                Console.WriteLine($"Found {itemType} convert to in {asciiValue} corrected for priority {asciiValue - 38}");
-            }
+            prioritiesSum += GetPriority(itemType);
         }
     }
 
 }
 
+int GetPriority(char input) {
+    //convert to ascii
+    var asciiValue = (int)input;
+    
+    //lowercase correction to priority
+    if(asciiValue > 96) {
+        return asciiValue - 96;
+    }
+    //uppercase correction to priority
+    else {
+        return asciiValue - 38;
+    }
+}
+
 Console.WriteLine($"The sum of all priorities is {prioritiesSum}");
+
+//part 2
+
+var rucksackGroupBadges = new List<int>();
+var rucksackGroup = new List<string>();
+foreach(var rucksack in input) {
+    if(rucksackGroup.Count < 2) {
+        rucksackGroup.Add(rucksack);
+    }
+    else {
+        //determine the badge itemtype
+        string itemsAlreadyFound = string.Empty;
+        foreach(var rucksackInGroup in rucksackGroup) {
+            string itemsAlreadyFoundInThisRucksack = string.Empty;
+            foreach(var itemType in rucksack) {
+                if(rucksackInGroup.Contains(itemType)) {
+                    itemsAlreadyFoundInThisRucksack += itemType;
+                }
+            }
+            //to prevent duplicates per rucksack
+            itemsAlreadyFound += new string(itemsAlreadyFoundInThisRucksack.Distinct().ToArray());
+        }
+        var itemTypeBadge = itemsAlreadyFound.GroupBy(x => x).OrderByDescending(o => o.Count()).First().First();
+        Console.WriteLine($"In group {rucksackGroup[0]}, {rucksackGroup[1]}, comparing with {rucksack} matching types are {itemsAlreadyFound}, which makes badge {itemTypeBadge}");
+        
+        rucksackGroupBadges.Add(GetPriority(itemTypeBadge));
+        //reset the group
+        rucksackGroup = new List<string>();
+    }
+}
+
+Console.WriteLine($"The sum of all priorities of the badge itemtypes is {rucksackGroupBadges.Sum()}");
